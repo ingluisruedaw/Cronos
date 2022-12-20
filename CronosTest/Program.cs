6933fc7464caac1d32a0d2a07cbb023b4587fc82
Luis Domingo Rueda Wilches <ingluisruedaw@gmail.com>
2022-12-20 04:45:39 -0500

    Finally adjust

---
+++ CronosTest/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cronos.LogExtension;
+
+Log l = new Log("Program", "Client", "CronosTest");
+
+l.Create(new Exception("Prueba"),"Demo");
+
+
+
+Console.WriteLine("writed log");
+Console.ReadKey();
